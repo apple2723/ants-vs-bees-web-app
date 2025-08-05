265d78294f5bb70adaae4abace3d63d242eef0d3
Thru-Echoes (Oliver Muellerklein) <omuellerklein@berkeley.edu>
2025-08-05 17:13:14 -0400

    Added sample gamestate instance and put all game changing logic in ants_engine.py

--- ants.py
+++ ants.py
@@ -1,239 +1,104 @@
 """Ants Vs. Bees (extension)"""
 
-from flask import Flask, jsonify
+import random
+from ucb import main, interact, trace
+from collections import OrderedDict
+#from ants_engine import *
+import ants_engine      # <-- if import just file, then use file name . class or function name (e.g. ants_engine.GameState())
 
-# --- Begin tiny REST API --- 
+from flask import Flask, jsonify, request
+
+#--------------------------------------------------
+# Make a sample gamestate instance 
+#--------------------------------------------------
+gs = ants_engine.GameState(
+    strategy = ants_engine.interactive_strategy,
+    beehive = ants_engine.Hive(ants_engine.make_normal_assault_plan()),
+    ant_types = ants_engine.ant_types(),
+    create_places = ants_engine.dry_layout,
+    dimensions=(3, 9),
+    food=2
+)
+
+#--------------------------------------------------
+# Begin tiny REST api via Flask  
+#--------------------------------------------------
 app = Flask(__name__)
 
+# TASK 1: How should the following route be changed to use the gamestate instance? 
+# 
 @app.route('/api/state')
 def api_state():
+    """
+    Can use this route to check values in game engine. 
+
+    In Terminal, execute with: 
+
+    curl localhost:5000/api/state
+    """
     return jsonify({
         'status' : 'ok',
         'time'   : 0,
         'food'   : 0,
-        'points' : 0    # <- placeholder values...
+        'points' : 0    
     })
 
 @app.route('/api/points')
 def api_points():
+    """
+    Can use this route to check point value in game engine. 
+
+    In Terminal, execute with: 
+
+    curl localhost:5000/api/points
+    """
     return jsonify({
-        'points' : GameState.points    # <- placeholder values...
+        'points' : GameState.points    # Task 2: ...and this one? 
     })
 
+# Task 3: How should this route be changed to use gamestate and correctly step through time? 
+# 
 @app.route('/api/time-step', methods = ["POST"])
 def api_time_step():
+    """
+    Can use this route to make the game engine increment +1 time. 
+
+    In Terminal, excute this route with:
+
+    curl -XPOST localhost:5000/api/time-step 
+    """
     GameState.time += 1
     return jsonify({
-        'time' : GameState.time    # <- placeholder values...
+        'time' : GameState.time    
     })
+
+@app.route('/api/deploy', methods=['POST'])
+def api_deploy():
+    """
+    Can use this route to test game engine directly or with Flask.
+
+    Via Terminal, call with:
+    #############################################################
+    curl -XPOST -H "Content-Type: application/json" \
+        -d '{"place" : "tunnel_0_0", "ant" : "Thrower"}' \
+        http://localhost:5000/api/deploy
+    """
+    data = request.get_json()
+    place_name = data['place']
+    ant_type   = data['ant']
+    try:
+        new_ant = gs.deploy_ant(place_name, ant_type)
+        return jsonify({ 'status': 'ok', 'ant_id': id(new_ant) })
+    except Exception as e:
+        # If error -> below is thrown 
+        return jsonify({ 'status': 'error', 'message': str(e) }), 400
 
 # NOTE: check very end of this file for last additional change!
 # --- END API hooks ---------
 
-import random
-from ucb import main, interact, trace
-from collections import OrderedDict
-from ants_engine import *
-
-class Water(Place):
-    """Water is a place that can only hold waterproof insects."""
-
-    def add_insect(self, insect):
-        """Add an Insect to this place. If the insect is not waterproof, reduce
-        its health to 0."""
-        # BEGIN Problem 10
-        "*** YOUR CODE HERE ***"
-        insect.add_to(self)
-        if insect.is_waterproof == False:
-            insect.reduce_health(insect.health)
-        # END Problem 10
-
-
-
-class AntRemover(Ant):
-    """Allows the player to remove ants from the board in the GUI."""
-
-    name = 'Remover'
-    is_implemented = False
-
-    def __init__(self):
-        super().__init__(0)
-
-
-class Hive(Place):
-    """The Place from which the Bees launch their assault.
-
-    assault_plan -- An AssaultPlan; when & where bees enter the colony.
-    """
-    is_hive = True
-
-    def __init__(self, assault_plan):
-        self.name = 'Hive'
-        self.assault_plan = assault_plan
-        self.bees = []
-        for bee in assault_plan.all_bees:
-            self.add_insect(bee)
-        # The following attributes are always None for a Hive
-        self.entrance = None
-        self.ant = None
-        self.exit = None
-
-    def strategy(self, gamestate):
-        exits = [p for p in gamestate.places.values() if p.entrance is self]
-        for bee in self.assault_plan.get(gamestate.time, []):
-            bee.move_to(random.choice(exits))
-            gamestate.active_bees.append(bee)
-
-
-class GameState:
-    """An ant collective that manages global game state and simulates time.
-
-    Attributes:
-    time -- elapsed time
-    food -- the colony's available food total
-    places -- A list of all places in the colony (including a Hive)
-    bee_entrances -- A list of places that bees can enter
-    """
-
-    def __init__(self, strategy, beehive, ant_types, create_places, dimensions, food=2, canhavequeenant=True, points=50):
-        """Create an GameState for simulating a game.
-
-        Arguments:
-        strategy -- a function to deploy ants to places
-        beehive -- a Hive full of bees
-        ant_types -- a list of ant classes
-        create_places -- a function that creates the set of places
-        dimensions -- a pair containing the dimensions of the game layout
-        """
-        self.time = 0
-        self.food = food
-        self.strategy = strategy
-        self.beehive = beehive
-        self.ant_types = OrderedDict((a.name, a) for a in ant_types)
-        self.dimensions = dimensions
-        self.active_bees = []
-        self.configure(beehive, create_places)
-        self.canhavequeenant = canhavequeenant
-        self.points = points
-
-    def configure(self, beehive, create_places):
-        """Configure the places in the colony."""
-        self.base = AntHomeBase('Ant Home Base')
-        self.places = OrderedDict()
-        self.bee_entrances = []
-        def register_place(place, is_bee_entrance):
-            self.places[place.name] = place
-            if is_bee_entrance:
-                place.entrance = beehive
-                self.bee_entrances.append(place)
-        register_place(self.beehive, False)
-        create_places(self.base, register_place, self.dimensions[0], self.dimensions[1])
-
-    def simulate(self):
-        """Simulate an attack on the ant colony (i.e., play the game)."""
-        num_bees = len(self.bees)
-        try:
-            while True:
-                print(f"[ants.py] simulate function Time: {self.time}, Points: {self.points}, Food: {self.food}")
-                self.beehive.strategy(self)         # Bees invade
-                self.strategy(self)                 # Ants deploy
-                for ant in self.ants:               # Ants take actions
-                    if ant.health > 0:
-                        ant.action(self)
-                for bee in self.active_bees[:]:     # Bees take actions
-                    if bee.health > 0:
-                        bee.action(self)
-                    if bee.health <= 0:
-                        num_bees -= 1
-                        self.active_bees.remove(bee)
-                        #Destroying a bee gives 5 points
-                        self.points +=5
-                if num_bees == 0:
-                    raise AntsWinException()
-                self.time += 1
-                # Points decrease by 1 when time goes up by 1
-                self.points -= 1
-        except AntsWinException:
-            print(f'All bees are vanquished. Your score: {self.points}. You win!')
-            return True
-        except AntsLoseException:
-            print('The ant queen has perished. Please try again.')
-            return False
-
-    def deploy_ant(self, place_name, ant_type_name):
-        """Place an ant if enough food is available.
-
-        This method is called by the current strategy to deploy ants.
-        """
-        constructor = self.ant_types[ant_type_name]
-        if self.food < constructor.food_cost:
-            print('Not enough food remains to place ' + ant_type_name)
-            raise NotEnoughFoodException()
-        if constructor.__name__ == 'QueenAnt':
-            try:
-                ant = constructor(self)
-            except DuplicateQueensException:
-                print("you cannot create a second QueenAnt")
-        else:
-            ant = constructor()
-
-        if ant:
-            self.places[place_name].add_insect(ant)
-            self.food -= ant.food_cost
-            return ant
-
-    def remove_ant(self, place_name):
-        """Remove an Ant from the game."""
-        #print (f"[ants.py] remove_ant function, Ant dies, point -7 :{self.points}")
-        place = self.places[place_name]
-        if place.ant is not None:
-            place.remove_insect(place.ant)
-
-    @property
-    def ants(self):
-        return [p.ant for p in self.places.values() if p.ant is not None]
-
-    @property
-    def bees(self):
-        return [b for p in self.places.values() for b in p.bees]
-
-    @property
-    def insects(self):
-        return self.ants + self.bees
-
-    def __str__(self):
-        status = ' (Food: {0}, Time: {1})'.format(self.food, self.time)
-        return str([str(i) for i in self.ants + self.bees]) + status
-
-class AntHomeBase(Place):
-    """AntHomeBase at the end of the tunnel, where the queen resides."""
-
-    def add_insect(self, insect):
-        """Add an Insect to this Place.
-
-        Can't actually add Ants to a AntHomeBase. However, if a Bee attempts to
-        enter the AntHomeBase, a AntsLoseException is raised, signaling the end
-        of a game.
-        """
-        assert isinstance(insect, Bee), 'Cannot add {0} to AntHomeBase'
-        raise AntsLoseException()
-
-def ants_win():
-    """Signal that Ants win."""
-    raise AntsWinException()
-
-def ants_lose():
-    """Signal that Ants lose."""
-    raise AntsLoseException()
-
-def ant_types():
-    """Return a list of all implemented Ant classes."""
-    all_ant_types = []
-    new_types = [Ant]
-    while new_types:
-        new_types = [t for c in new_types for t in c.__subclasses__()]
-        all_ant_types.extend(new_types)
-    return [t for t in all_ant_types if t.is_implemented]
+#--------------------------------------------------
+# Exceptions (need to figure out place for these...) 
+#--------------------------------------------------
 
 class GameOverException(Exception):
     """Base game over Exception."""
@@ -254,61 +119,6 @@ class DuplicateQueensException(Exception):
 class NotEnoughFoodException(Exception):
     """Occurs when there is not enough food to create an Ant."""
     pass
-
-def interactive_strategy(gamestate):
-    """A strategy that starts an interactive session and lets the user make
-    changes to the gamestate.
-
-    For example, one might deploy a ThrowerAnt to the first tunnel by invoking
-    gamestate.deploy_ant('tunnel_0_0', 'Thrower')
-    """
-    print('gamestate: ' + str(gamestate))
-    msg = '<Control>-D (<Control>-Z <Enter> on Windows) completes a turn.\n'
-    interact(msg)
-
-###########
-# Layouts #
-###########
-
-def wet_layout(queen, register_place, tunnels=3, length=9, moat_frequency=3):
-    """Register a mix of wet and and dry places."""
-    for tunnel in range(tunnels):
-        exit = queen
-        for step in range(length):
-            if moat_frequency != 0 and (step + 1) % moat_frequency == 0:
-                exit = Water('water_{0}_{1}'.format(tunnel, step), exit)
-            else:
-                exit = Place('tunnel_{0}_{1}'.format(tunnel, step), exit)
-            register_place(exit, step == length - 1)
-
-def dry_layout(queen, register_place, tunnels=3, length=9):
-    """Register dry tunnels."""
-    wet_layout(queen, register_place, tunnels, length, 0)
-
-
-#################
-# Assault Plans #
-#################
-
-class AssaultPlan(dict):
-    """The Bees' plan of attack for the colony.  Attacks come in timed waves.
-
-    An AssaultPlan is a dictionary from times (int) to waves (list of Bees).
-
-    >>> AssaultPlan().add_wave(4, 2)
-    {4: [Bee(3, None), Bee(3, None)]}
-    """
-
-    def add_wave(self, bee_type, bee_health, time, count):
-        """Add a wave at time with count Bees that have the specified health."""
-        bees = [bee_type(bee_health) for _ in range(count)]
-        self.setdefault(time, []).extend(bees)
-        return self
-
-    @property
-    def all_bees(self):
-        """Place all Bees in the beehive and return the list of Bees."""
-        return [bee for wave in self.values() for bee in wave]
 
 
 # * Last change added to setup basic Flask api endpoint routing * 
